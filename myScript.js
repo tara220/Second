44c3ee819d35245d55b5e70379b76f9249899c61
Kiana <soltany.2019@gmail.com>
2020-12-27 09:58:06 +0330

    Second

---
+++ myScript.js
@@ -0,0 +1,27 @@
+//function myFunction() {
+//  var x = document.getElementById("myDIV");
+ // if (x.style.display === "none") {
+  //  x.style.display = "block";
+ // } else {
+//    x.style.display = "none";
+//  }
+//}
+
+// Validating Empty Field
+
+function myFunction() {
+    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us7.list-manage.com","uuid":"d5b6774fb8bf432d69df52d93","lid":"3ed431a3b6"}) });
+ }
+
+
+var a;
+function myFunction(){
+  if(a==1){
+    document.getElementById("image").style.display="inline";
+    return a=0;
+    }
+   else{
+     document.getElementById("image").style.display="none";
+     return a=1;
+   } 
+}
